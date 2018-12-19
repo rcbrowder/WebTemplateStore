3da7486d6f92544b043ed268691a969a8ff3daee
Chris Browder <rcbrowder@gmail.com>
2018-12-19 15:51:00 -0500

    Created routes and used angular to dynamically change data.

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -5,9 +5,26 @@ angular.module('templateStore.templates',['ngRoute'])
     .when('/templates', {
       templateUrl: 'templates/templates.html',
       controller: 'TemplatesCtrl'
-    });
+    })
+    .when('/templates/:templateid', {
+      templateUrl: 'templates/template-details.html',
+      controller: 'TemplateDetailsCtrl'
+    })
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope){
-  console.log($scope);
+.controller('TemplatesCtrl', ['$scope', "$http", function($scope, $http){
+  $http.get('json/templates.json').then(function(response){
+    $scope.templates = response.data;
+  });
+
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+  var templateId = $routeParams.templateid;
+  $http.get('json/templates.json').then(function(response){
+    $scope.template = $filter('filter')(response.data, function(d){
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+  });
 }]);
